93ffe1aab6b38e6697164f68ad7b1adb22152a6c
Mrgreen <danieliosifogrin@gmail.com>
2026-07-14 11:46:54 +0300

    Modul 4: mining module (PoW) - find nonce brute-force

---
+++ core/include/mining.h
@@ -0,0 +1,10 @@
+#ifndef MINING_H
+#define MINING_H
+
+#include "block.h"
+
+#define DIFFICULTY 4
+
+void mine_block(Block* block, int difficulty);
+
+#endif // !MINING_H
---
+++ core/src/mining.cpp
@@ -0,0 +1,28 @@
+#include "mining.h"
+#include <cstdio>
+#include <ctime>
+
+static int hash_meets_difficulty(const char* hash, int difficulty) {
+	for (int i = 0; i < difficulty; i++){
+		if (hash[i] == '0') {
+			return 0;
+		}
+	}
+	return 1;
+}
+
+void mine_block(Block* block, int difficulty) {
+	clock_t start = clock();
+	block->nonce = 0;
+	compute_hash(block);
+
+	while (!hash_meets_difficulty(block->hash, difficulty)) {
+		block->nonce++;
+		compute_hash(block);
+	}
+
+	clock_t end=clock();
+	double seconds = (double)(end - start) / CLOCKS_PER_SEC;
+
+	printf("Bloc #%u minat: nonce=%u, timp=%.4f sec\n", block->index, block->nonce, seconds);
+}
